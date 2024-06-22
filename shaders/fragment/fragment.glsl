155b4b4a5866d7016ca1f92ced7187a0edf65324
Chris Gruber <gruber.christopher@gmail.com>
2024-06-21 20:16:45 -0500

    interpolated shaded square

--- shaders/fragment/fragment.glsl
+++ shaders/fragment/fragment.glsl
@@ -1,7 +1,10 @@
 #version 330 core
-layout(location = 0) out vec4 color;
+
+in vec3 color;
+
+out vec4 out_color;
 
 void main()
 {
-    color = vec4(1.0, 0.0, 0.0, 1.0);
+    out_color = vec4(color, 1.0);
 }
--- shaders/vertex/vertex.glsl
+++ shaders/vertex/vertex.glsl
@@ -1,7 +1,10 @@
 #version 330 core
 layout(location = 0) in vec4 position;
 
+out vec3 color;
+
 void main()
 {
     gl_Position = position;
+    color = vec3(position.x + 0.5, 1.0, position.y + 0.5);
 }
